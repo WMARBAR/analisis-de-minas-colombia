e4cdc187d73df6c8d15522434066a06945f64580
Wilson Martinez <wmartinez0.01666@gmail.com>
2022-03-22 10:41:15 -0500

    2.) conexion con la base de datos y limpieza de la columna 1

--- Minas_colombia.py
+++ Minas_colombia.py
@@ -2,3 +2,8 @@ import pandas as pd
 import datetime
 import matplotlib.pyplot as plt
 import numpy as np
+
+#Agregar base de datos de minas del gobierno de colombia
+df_minas = pd.read_excel("D:/WILSON MARTINEZ/programas/python/Analisis_Minas/Base_minas_colombia_analisis.xlsx",sheet_name="Evento_minas_RAW")
+#Arreglo de caracteres con espacio el la columna "presunto actor responsable"
+df_minas['Presunto actor responsable']=df_minas['Presunto actor responsable'].str.strip(' ')
