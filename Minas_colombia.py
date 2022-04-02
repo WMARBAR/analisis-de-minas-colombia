822c7cb9bda1be7e1d1cad4c4a15f240433d14b5
Wilson Martinez <wmartinez0.01666@gmail.com>
2022-04-02 16:45:46 -0500

    7.) añadiendo el calendario global

--- Minas_colombia.py
+++ Minas_colombia.py
@@ -5,6 +5,11 @@ import numpy as np
 
 #Agregar base de datos de minas del gobierno de colombia
 df_minas = pd.read_excel("D:/WILSON MARTINEZ/programas/python/Analisis_Minas/Base_minas_colombia_analisis.xlsx",sheet_name="Evento_minas_RAW")
+
+#Agregando la informacion del calendario
+df_GLOBAL_CALENDAR = pd.read_excel("D:/WILSON MARTINEZ/programas/python/CALENDAR_GLOBAL.xlsx",sheet_name="Calendar")
+df_GLOBAL_CALENDAR= df_GLOBAL_CALENDAR.rename(columns={'date':'row_date'})
+
 #Arreglo de caracteres con espacio el la columna "presunto actor responsable"
 df_minas['Presunto actor responsable']=df_minas['Presunto actor responsable'].str.strip(' ')
 
